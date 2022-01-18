77cdb53f1cf6b8cbed21355d62293c3323515dc4
Viacheslav Yuskevych <viacheslav.yuskevych@globallogic.com>
2022-01-18 11:37:46 +0200

    form control for todo

--- src/app/modules/pages/modules/todos/todos.component.ts
+++ src/app/modules/pages/modules/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from 'src/app/modules/pages/modules/todos/todos.service';
 import { UsersService } from 'src/app/core/services/users.service';
+import { FormControl, Validators } from '@angular/forms';
 
 export interface ITodo {
   content: string;
@@ -15,17 +16,29 @@ export interface ITodo {
 export class TodosComponent {
   users = [];
 
+  public control = new FormControl(null, [
+    Validators.required,
+    Validators.minLength(5),
+  ]);
+
   constructor(
     private readonly todoService: TodoService,
     private readonly usersService: UsersService
   ) {}
 
   public todos = this.todoService.todos;
-  public addTodo = this.todoService.addTodo;
 
   public loadUsers() {
     this.usersService.getAllUsers().subscribe((response) => {
       console.log(response);
     });
+  }
+
+  public addTodo() {
+    const content: string = this.control.value;
+
+    this.todoService.addTodo({ content, done: false });
+
+    this.control.reset();
   }
 }
--- src/app/modules/pages/modules/todos/todos.module.ts
+++ src/app/modules/pages/modules/todos/todos.module.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { TodosRoutingModule } from './todos-routing.module';
 import { TodosComponent } from './todos.component';
 import { TodosItemsComponent } from '../components/todos-items/todos-items.component';
 import { TodosItemComponent } from '../components/todos-item/todos-item.component';
 
-
-
 @NgModule({
-  declarations: [
-    TodosComponent,
-    TodosItemsComponent,
-    TodosItemComponent,
-  ],
-  imports: [
-    CommonModule,
-    TodosRoutingModule
-  ]
+  declarations: [TodosComponent, TodosItemsComponent, TodosItemComponent],
+  imports: [CommonModule, TodosRoutingModule, FormsModule, ReactiveFormsModule],
 })
-export class TodosModule { }
+export class TodosModule {}
